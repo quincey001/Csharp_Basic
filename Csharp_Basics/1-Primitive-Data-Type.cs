53b1e47dc500de7989ee1ff6584fcc9d90e0305a
chris <yancongqin@gmail.com>
2024-02-03 03:45:56 +0000

    Create README.md
    add readme

---
+++ Csharp_Basics/1-Primitive-Data-Type.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Csharp_Basics
+{
+	public class EmptyClass
+	{
+		 
+		public static void PrimitiveData()
+		{
+            // integer: default is int
+            sbyte a = -0b111;
+            short b = -0x334;
+            int c = -0b101010;
+            long d = -0x3;
+            Console.WriteLine($"this is a signed byte: {a}");
+            Console.WriteLine($"this is a signed short: {b}");
+            Console.WriteLine($"this is a signed int: {c}");
+            Console.WriteLine($"this is a signed long: {d}");
+        }
+	}
+}
+
--- Csharp_Basics/Program.cs
+++ Csharp_Basics/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Csharp_Basics;
 
+EmptyClass.PrimitiveData();
